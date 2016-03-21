82bf70867521690018dcc80e1aaeb9cc96b4c32f
zhoushx3 <1006487900@qq.com>
2016-03-22 00:00:27 +0800

    添加旋转和透明度组件

--- src/component/EditorList/Textpanel.jsx
+++ src/component/EditorList/Textpanel.jsx
@@ -1,7 +1,9 @@
 import React from 'React'
 import Font from './Font.jsx'
+import Size from './Size.jsx'
 import Position from './Position.jsx'
 import TextAlign from './TextAlign.jsx'
+import Percentage from './Percentage.jsx'
 
 import ElementAction from '../../action/ElementAction.js'
 
@@ -31,11 +33,13 @@ const TextPanel = React.createClass({
 			<div id="text-panel">
 				<Font element={ element } setContent={ this.setContent } setStyle={ this.setStyle } />
 				<TextAlign textAlign={ element.style.textAlign } setStyle={ this.setStyle } />
+				<Size width={ element.style.width } height={ element.style.height } setStyle={ this.setStyle } />
 				<Position left={ element.style.left } top={ element.style.top } setStyle={ this.setStyle } />
+				<Percentage type="opacity" value={ element.style.opacity } setStyle={ this.setStyle } />
+				<Percentage type="rotate" value={ element.style.transform } setStyle={ this.setStyle } />
 			</div>
 		)	
 	}
 })
 
 export default TextPanel
-				// <Scale element={ element }/>

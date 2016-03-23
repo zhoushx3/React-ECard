f0a6799a5f734d617466261cbc3c9595a800cbf4
zhoushx3 <1006487900@qq.com>
2016-03-24 01:35:07 +0800

    添加深复制和深比较

--- src/util/func.js
+++ src/util/func.js
@@ -9,3 +9,52 @@ export function lower(n, limit) {
 export function upper(n, limit) {
 	return n <= limit ? n : limit
 }
+
+// 拷贝对象
+export function deepCopy(source) {
+	if ( typeof source !== 'object' || source === null)
+		return source
+
+	let isArr = isArray(source)
+	let obj = isArr ? [] : {}
+	for ( let key in source ) {
+			key = isArr ? parseInt(key) : key
+			obj[key] = deepCopy( source[key] )
+	}
+	return obj
+}
+
+// 比较 true: 值相同
+export function deepCompare(comp1, comp2) {
+	if ( typeof comp1 !== 'object' || comp1 === null ) {
+		return comp1 === comp2
+	}
+	let isArr1 = isArray(comp1)
+	let isArr2 = isArray(comp2)
+	// 若都是数组
+	if ( isArr1 && isArr2 ) {
+		if ( comp1.length !== comp2.length ) return false
+			
+		for ( let i = 0; i < comp1.length; i += 1 )
+			if ( !deepCompare(comp1[i], comp2[i]) ) return false
+
+		return true
+	}
+	// 若都不是数组的对象
+	if ( !isArr1 && !isArr2 ) {
+		if ( Object.keys(comp1).length !== Object.keys(comp2).length ) return false
+
+		for ( let key in comp1 ) {
+			if ( !( key in comp2 ) ) return false
+			if ( !deepCompare(comp1[key], comp2[key]) ) return false
+		}
+
+		return true
+	}
+	// 一数组一非数组
+	return false
+}
+
+function isArray(obj) {  
+  return Object.prototype.toString.call(obj) === '[object Array]';   
+}
